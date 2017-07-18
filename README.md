4f5e88bd83d50c43a77832d7fd2c8fb13aa55cb1
Robert Manukyan <robertmanukyan@Roberts-MBP.attlocal.net>
2017-07-17 20:23:15 -0700

    set up

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# Tradesy Frontend Styleguide
-
-- Server / IP: 107.170.198.91
-- UN: root
-- PW: Tradesy621# hercules
+# Hercules Framework
---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./css/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css/public'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./css/scss/*.scss', ['sass']);
+});
